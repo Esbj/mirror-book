eda5ab4f191d2c1d3d44bd1ac7cc99a9d67dd1f7
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-08-25 09:55:17 +0200

    re-factor to single source of truth

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -9,15 +9,15 @@ const Form: React.FC<Props> = ({ updateMessage }) => {
 
   //single source of truth, react / state ska äga inehållet, inte formuläret
   const [header, setHeader] = useState("")
-  const [body, setBody] = useState("")
+  const [message, setMessage] = useState("")
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    updateMessage(header, body)
+    updateMessage(header, message)
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder="Rubrik..." type="text" name="header" id="header" onChange={(e) => setHeader(e.target.value)} />
-      <textarea placeholder='Skriv något kul' name="body" id="body" onChange={(e) => setBody(e.target.value)} />
+      <input placeholder="Rubrik..." value={header} type="text" name="header" id="header" onChange={(e) => setHeader(e.target.value)} />
+      <textarea placeholder='Skriv något kul' value={message} name="body" id="body" onChange={(e) => setMessage(e.target.value)} />
       <button type="submit">Spara</button>
     </form>
   )
