6bfcead7ea826ba171542c5c694840ea6fd229d5
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-08-25 09:41:00 +0200

    rename and added comment

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -6,16 +6,18 @@ type Props = {
 }
 const Form: React.FC<Props> = ({ updateMessage }) => {
 
-  const [header, setLocalHeader] = useState("")
-  const [body, setLocalBody] = useState("")
+
+  //single source of truth, react / state ska äga inehållet, inte formuläret
+  const [header, setHeader] = useState("")
+  const [body, setBody] = useState("")
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     updateMessage(header, body)
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder="Rubrik..." type="text" name="header" id="header" onChange={(e) => setLocalHeader(e.target.value)} />
-      <textarea placeholder='Skriv något kul' name="body" id="body" onChange={(e) => setLocalBody(e.target.value)} />
+      <input placeholder="Rubrik..." type="text" name="header" id="header" onChange={(e) => setHeader(e.target.value)} />
+      <textarea placeholder='Skriv något kul' name="body" id="body" onChange={(e) => setBody(e.target.value)} />
       <button type="submit">Spara</button>
     </form>
   )
